28fcb18ee816e629a94faebe9c9aab318b9c88b2
Tim Curless <tim.curless@gmail.com>
2017-06-10 21:04:49 -0500

    adding middlewares

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 /* eslint consistent-return:0 */
 const express = require('express');
 const logger = require('./logger');
+const resolve = require('path').resolve;
+const setup = require('./middlewares/frontendMiddleware');
 const argv = require('minimist')(process.argv.slice(2));
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null;
 const prettyHost = customHost || 'localhost';
 const port = argv.port || process.env.PORT || 3000;
 const app = express();
+
+setup(app, {
+  outputPath: resolve(process.cwd(), 'build'),
+  publicPath: '/',
+});
 
 app.listen(port, host, (err) => {
   if (err) {
---
+++ server/middlewares/frontendMiddleware.js
@@ -0,0 +1,4 @@
+/* eslint-disable global-require */
+
+
+// Dev Middleware
